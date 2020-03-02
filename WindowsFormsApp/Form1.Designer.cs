178605a09e58979e8f26ffdf2cc95e5e482a51f0
kami_teru <teru.k.business@gmail.com>
2020-03-02 11:53:21 +0900

    プロジェクト ファイルを追加します。

---
+++ WindowsFormsApp/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace WindowsFormsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Sleep1Button = new System.Windows.Forms.Button();
+            this.Sleep2Button = new System.Windows.Forms.Button();
+            this.Sleep3Button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Sleep1Button
+            // 
+            this.Sleep1Button.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Sleep1Button.Font = new System.Drawing.Font("MS UI Gothic", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Sleep1Button.Location = new System.Drawing.Point(91, 45);
+            this.Sleep1Button.Name = "Sleep1Button";
+            this.Sleep1Button.Size = new System.Drawing.Size(435, 83);
+            this.Sleep1Button.TabIndex = 0;
+            this.Sleep1Button.Text = "Sleep3000";
+            this.Sleep1Button.UseVisualStyleBackColor = true;
+            this.Sleep1Button.Click += new System.EventHandler(this.Sleep1Button_Click);
+            // 
+            // Sleep2Button
+            // 
+            this.Sleep2Button.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Sleep2Button.Font = new System.Drawing.Font("MS UI Gothic", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Sleep2Button.Location = new System.Drawing.Point(91, 152);
+            this.Sleep2Button.Name = "Sleep2Button";
+            this.Sleep2Button.Size = new System.Drawing.Size(435, 83);
+            this.Sleep2Button.TabIndex = 1;
+            this.Sleep2Button.Text = "Sleep3000 BGWorker";
+            this.Sleep2Button.UseVisualStyleBackColor = true;
+            this.Sleep2Button.Click += new System.EventHandler(this.Sleep2Button_Click);
+            // 
+            // Sleep3Button
+            // 
+            this.Sleep3Button.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Sleep3Button.Font = new System.Drawing.Font("MS UI Gothic", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Sleep3Button.Location = new System.Drawing.Point(91, 257);
+            this.Sleep3Button.Name = "Sleep3Button";
+            this.Sleep3Button.Size = new System.Drawing.Size(435, 83);
+            this.Sleep3Button.TabIndex = 2;
+            this.Sleep3Button.Text = "Sleep3000 Async";
+            this.Sleep3Button.UseVisualStyleBackColor = true;
+            this.Sleep3Button.Click += new System.EventHandler(this.Sleep3Button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(620, 390);
+            this.Controls.Add(this.Sleep3Button);
+            this.Controls.Add(this.Sleep2Button);
+            this.Controls.Add(this.Sleep1Button);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Sleep1Button;
+        private System.Windows.Forms.Button Sleep2Button;
+        private System.Windows.Forms.Button Sleep3Button;
+    }
+}
+
---
+++ WindowsFormsApp/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Sleep1Button_Click(object sender, EventArgs e)
+        {
+            var button = sender as Button;
+            button.Enabled = false;
+
+            Thread.Sleep(3000);
+
+            button.Enabled = true;
+        }
+
+        private void Sleep2Button_Click(object sender, EventArgs e)
+        {
+            var button = sender as Button;
+            button.Enabled = false;
+            var bgworker = new BackgroundWorker();
+            bgworker.DoWork += (s, ev) =>
+            {
+                Thread.Sleep(3000);
+            };
+            bgworker.RunWorkerCompleted += (s, ev) =>
+            {
+                button.Enabled = true;
+            };
+            bgworker.RunWorkerAsync();
+        }
+
+        private async void Sleep3Button_Click(object sender, EventArgs e)
+        {
+            var button = sender as Button;
+            button.Enabled = false;
+            await Task.Run(() => {
+                Thread.Sleep(3000);
+            });
+            button.Enabled = true;
+        }
+    }
+}
